209bca65320a3ba744050efc12b3ac801e764326
AnishMukherjee03 <AnishMukherjee03@users.noreply.github.com>
2021-08-08 22:56:18 +0530

    Added a skeleton business list to be later filled by Yelp API

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './BusinessList.css';
+import Business from '..Business/Business.js';
+
+export class BusinessList extends React.Component {
+	render() {
+		return (
+			<div class="BusinessList">
+			  	<!-- Add Business Component here --> 
+  				<!-- Add Business Component here -->
+  				<!-- Add Business Component here -->
+  				<!-- Add Business Component here -->
+  				<!-- Add Business Component here -->
+  				<!-- Add Business Component here -->
+			</div>
+		)
+	}
+};
+
+
+
